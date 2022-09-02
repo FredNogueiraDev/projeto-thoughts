ef4e7d9a903e72ef9e878813f1853e39b0013fd9
Frederico Nogueira <fredericonogueira01@outlook.com>
2022-09-02 09:27:08 -0300

    iniciando estrutura do projeto

---
+++ db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize, Model } = require('sequelize');
+
+const sequelize = new Sequelize('toughts', 'root', 'admin123', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+try {
+    sequelize.authenticate()
+    console.log('Conexão com BD realizada com Sucesso!')
+}catch(err){
+    console.log(`Não foi possível conectar ao BD: ${err}`)
+}
+
+module.exports = sequelize
