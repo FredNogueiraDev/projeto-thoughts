c32b5492101c0debf14ee26c62e6238da74a5715
Frederico Nogueira <fredericonogueira01@outlook.com>
2022-09-05 10:21:08 -0300

    Finalizando autenticação

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,6 +6,39 @@ module.exports = class AuthController {
     static login (req, res){
         res.render('auth/login')
     }
+
+    static async loginPost(req, res){
+        const { email, password} = req.body;
+
+        // find user
+        const user = await User.findOne({where: {email: email}})
+
+        if(!user){
+            req.flash('message', 'Usuário ou senha incorretos!')
+            res.render('auth/login')
+
+            return
+        }
+
+        // check if passwords match
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+
+
+        if(!passwordMatch){
+            req.flash('message', 'Usuário ou senha incorretos!')
+            res.render('auth/login')
+
+            return
+        }
+
+        // initialize session
+        req.session.userid = user.id
+                        
+        req.session.save(() => {
+            res.redirect('/')
+        })
+
+    }
     
     static register (req, res){
         res.render('auth/register')
